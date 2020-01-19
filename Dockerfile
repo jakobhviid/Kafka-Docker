367fe395da0afbaf37b5c2773cf5ae9e4fd57939
Oliver Marco van Komen <olvan18@student.sdu.dk>
2020-01-19 16:51:36 +0100

    Working kafka + Example of docker-compose

--- Dockerfile
+++ Dockerfile
@@ -6,21 +6,21 @@ RUN apt-get update && \
     apt-get install -y --no-install-recommends openjdk-8-jre-headless && \
     apt-get install -y jq
 
-# Test if mirror is working and use other mirrors if the optimal ones are down
-ADD https://archive.apache.org/dist/kafka/2.4.0/kafka_2.12-2.4.0.tgz /opt/
-
-# Install kafka
-RUN cd /opt && \
-    tar -xzf kafka_2.12-2.4.0.tgz && \
-    mv kafka_2.12-2.4.0 kafka && \
-    rm -rf /opt/*.tgz && \
-    mkdir ${KAFKA_HOME}/data
-
-# Setup necessary scripts 
-COPY scripts /tmp/
+# Copy necessary scripts + configuration
+COPY scripts server.properties /tmp/
 RUN chmod +x /tmp/*.sh && \
     mv /tmp/*.sh /usr/bin && \
-    rm -rf /tmp/*
+    rm -rf /tmp/*.sh
+
+# Install kafka
+# ADD https://archive.apache.org/dist/kafka/2.4.0/kafka_2.12-2.4.0.tgz /opt/
+COPY ./kafka_2.12-2.4.0.tar /opt/
+RUN cd /opt && \
+    tar -xf kafka_2.12-2.4.0.tar && \
+    mv kafka_2.12-2.4.0 kafka && \
+    rm -rf /opt/*.tar && \
+    mv /tmp/server.properties /opt/kafka/config/server.properties && \
+    mkdir /data && mkdir /data/kafka
 
 HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD [ "healthcheck.sh" ]
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,12 @@ if [ -z "$KAFKA_ZOOKEEPER_CONNECT" ]
 then 
     echo "ERROR Missing essential zookeeper connection URI"
     exit 1
+else
+    if [[ "$KAFKA_ZOOKEEPER_CONNECT" != *"/kafka"* ]]
+    then
+        echo "INFO KAFKA_ZOOKEEPER_CONNECT missing chroot suffix, adding default /kafka chroot"
+        KAFKA_ZOOKEEPER_CONNECT="$KAFKA_ZOOKEEPER_CONNECT/kafka"
+    fi
 fi
 
 if [ -z "$KAFKA_BROKER_ID" ]
@@ -30,15 +36,23 @@ then
     echo "INFO Missing listener security protocol map, using default " $KAFKA_LISTENER_SECURITY_PROTOCOL_MAP
 fi
 
+if [ -z "$KAFKA_RETENTION_HOURS" ]
+then
+    KAFKA_RETENTION_HOURS=168
+    echo "INFO Missing retention hours configuration. Using default of 7 days " $KAFKA_RETENTION_HOURS
+fi
+
+echo "INFO Configuring Kafka Server"
+
+echo "* hard nofile 100000
+* soft nofile 100000" | tee --append /etc/security/limits.conf
+
 echo "INFO Starting Kafka Server"
-echo "A LISTERNESR " $KAFKA_ADVERTISED_LISTENERS
-echo "LISTENERS " $KAFKA_LISTENERS
 
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties \
 --override zookeeper.connect="$KAFKA_ZOOKEEPER_CONNECT" \
 --override broker.id="$KAFKA_BROKER_ID" \
---override log.dirs="$KAFKA_HOME"/data \
 --override advertised.listeners="$KAFKA_ADVERTISED_LISTENERS" \
 --override listeners="$KAFKA_LISTENERS" \
 --override listener.security.protocol.map="$KAFKA_LISTENER_SECURITY_PROTOCOL_MAP" \
---override inter.broker.listener.name=INSIDE
+--override log.retention.hours="$KAFKA_RETENTION_HOURS"
