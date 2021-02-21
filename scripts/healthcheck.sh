c24daeabcba5e125cc8d07eb719b77e94fa6b9d2
Daniel Skjold Toft <dstoft@hotmail.com>
2021-02-21 15:12:34 +0100

    Healthcheck updated
    Healthcheck will not delete the healthcheck topic created, and it will not attempt to create a new one if it finds one initially.

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -6,22 +6,26 @@ kafkaProcess=$(ps -x | grep java | grep kafka)
 # If kafkaProcess is not empty
 if ! [[ -z "$kafkaProcess" ]]; then
     # Check logic. Healthcheck will time out if server is not available.
-    topicName="TEST-HEALTHCHECK-BROKER-$KAFKA_BROKER_ID"
+    topicName="HEALTHCHECK-BROKER-$KAFKA_BROKER_ID"
     KAFKA_HEALTHCHECK_PORT=${KAFKA_HEALTHCHECK_PORT:-9092}
     
-    /opt/kafka/bin/kafka-topics.sh --create --topic $topicName --bootstrap-server localhost:$KAFKA_HEALTHCHECK_PORT --partitions 1 --replication-factor 1
-
-    # If somehow the topic already exists it will throw an error when creating, but if this checks goes through that means the server is still operating fine and will therefore not be considered unhealthy
+    # Fetch topic information
     topic=$(/opt/kafka/bin/kafka-topics.sh --list --topic $topicName --bootstrap-server localhost:$KAFKA_HEALTHCHECK_PORT | grep "$topicName")
 
-    # If topic is empty
+    # If topic is empty, should only happen when starting a new kafka server
     if [[ -z "$topic" ]]; then
-        echo " ERROR Could not find created topic in list "
-        exit 1
+		#Create new topic
+		/opt/kafka/bin/kafka-topics.sh --create --topic $topicName --bootstrap-server localhost:$KAFKA_HEALTHCHECK_PORT --partitions 1 --replication-factor 1
+		
+		# Refetch topic information
+		topic=$(/opt/kafka/bin/kafka-topics.sh --list --topic $topicName --bootstrap-server localhost:$KAFKA_HEALTHCHECK_PORT | grep "$topicName")
+		
+		# If topic is still empty
+		if [[ -z "$topic" ]]; then
+			echo " ERROR Could not find healthcheck topic in list "
+			exit 1
+		fi
     fi
-
-    # Delete topic
-    /opt/kafka/bin/kafka-topics.sh --delete --topic $topicName --bootstrap-server localhost:$KAFKA_HEALTHCHECK_PORT
 
     echo " OK "
     exit 0
